2d93ae245d981f2145f24ac168f4e9ab8755f391
Luis Orellana <luisr.orellana@gmail.com>
2021-02-23 20:58:53 -0600

    ultima modificacion

---
+++ pregunta3/script.js
@@ -0,0 +1,9 @@
+var celdas = document.getElementsByTagName("Table");
+for(var i=0; i<celdas.length; i++){
+    var recuadro = document.createElement("td");
+    var border = document.border;
+    myNodelist[i].appendChild(recuadro);
+
+
+}
+
